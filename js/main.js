8ec2f7d0560fc116da8f81c5fb3cfc95943f242a
Sonus Sundar <sonusmsundar@gmail.com>
2016-07-14 09:59:14 +0530

    updates

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+$('.slider').slick({
+  dots: false,
+  infinite: false,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  arrows:false,
+  responsive:true
+});
+});
